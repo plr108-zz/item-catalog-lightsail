c2ae790a4180a4080ca40725e5f0fe9133917337
Patrick Roche <patrick.l.roche@gmail.com>
2018-02-07 18:09:33 -0500

    Initial commit
    Initial commit for Udacity Full Stack Web Developer Nanodegree Item Catalog Project.
    Add database setup.

---
+++ database_setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+    name = Column(String(80), nullable=False)
+    cat_id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'cat_id': self.cat_id,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
