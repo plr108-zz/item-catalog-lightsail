72e9c8b3073d318664a4e93cff816a24cea81db4
Patrick Roche <patrick.l.roche@gmail.com>
2018-02-20 11:52:37 -0500

    Rename google_id
    Change gplus_id references to google_id since Google Sign-In is now used for login

--- application.py
+++ application.py
@@ -111,8 +111,8 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
+    google_id = credentials.id_token['sub']
+    if result['user_id'] != google_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -125,15 +125,15 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     stored_access_token = login_session.get('access_token')
-    stored_gplus_id = login_session.get('gplus_id')
-    if stored_access_token is not None and gplus_id == stored_gplus_id:
+    stored_google_id = login_session.get('google_id')
+    if stored_access_token is not None and google_id == stored_google_id:
         response = make_response(
             json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
+    login_session['google_id'] = google_id
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
@@ -166,7 +166,7 @@ def gdisconnect():
         if result['status'] == '200':
             username = login_session.get('username')
             del login_session['access_token']
-            del login_session['gplus_id']
+            del login_session['google_id']
             del login_session['username']
             del login_session['email']
             flash_msg = username + ' has been logged out'
